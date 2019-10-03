e816d85703a5be620e34d6fa5f8e0e69cfc8fa4b
Eliezer Rodrigo <er.7santana@gmail.com>
2019-10-02 23:15:23 -0300

    Initial commit

---
+++ PWOverlayController/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  PWOverlayController
+//
+//  Created by Eliezer Rodrigo on 02/10/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- PWOverlayController/ViewController.swift
+++ PWOverlayController/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DTOverlayController
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - IBActions
+    
+    @IBAction func showOverlayController(_ sender: Any) {
+        
+        showOverlayController()
+    }
+    
+    //MARK: - Show Overlay Controller
+    
+    func showOverlayController() {
+        
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "secondViewController")
+        
+        let overlayController = DTOverlayController(viewController: viewController)
 
+        // View controller is automatically dismissed when you release your finger
+        overlayController.dismissableProgress = 0.4
+
+        // Enable/disable pan gesture
+        overlayController.isPanGestureEnabled = false
+
+        // Update top-left and top-right corner radius
+        overlayController.overlayViewCornerRadius = 20
+
+        // Control the height of the view controller
+        overlayController.overlayHeight = .dynamic(0.5) // 80% height of parent controller
+//        overlayController.overlayHeight = .static(300) // fixed 300-point height
+//        overlayController.overlayHeight = .inset(50) // fixed 50-point inset from top
+//
+        
+        present(overlayController, animated: true, completion: nil)
+    }
+    
 }
 
